403e9682edd7b776d7763f3039fdba208e9cd07b
Nathan Crincket <crincketnathan@gmail.com>
2024-09-18 15:45:43 +0200

    test-18/09/24

---
+++ tp_note/repeat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* repeat(int *t1, int *t2, int taille, int tailleFinale){
+
+    int *tab;
+    tab = malloc(tailleFinale);
+    int compteur = 0;
+
+
+    for(int i = 0 ; i < taille ; i++){
+        for(int j = 0 ; j < t1[i] ; j++){
+            tab[compteur] = t2[i];
+            compteur++;
+        }
+    }
+
+    return tab;
+}
+
+void afficheTab(int *tab, int taille){
+    printf("(");
+    for(int i = 0 ; i < taille ; i++){
+        printf(" %d ", tab[i]);
+    }
+    printf(")");
+}
+
+int main(void){
+    printf("Hello World\n");
+    int tab1[] = {1,2,3,5};
+    int tab2[] = {10,3,8,2};
+    int taille = 4;
+    int tailleFinale = 0;
+    for(int i = 0 ; i < taille; i++){
+        tailleFinale = tailleFinale + tab1[i];
+    }
+    int *resultat = repeat(tab1, tab2, taille, tailleFinale);
+    afficheTab(resultat, tailleFinale);
+}
