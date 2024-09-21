cab03ac62cfd516c8d11d5daac4a28a4352f0648
Nathan Crincket <crincketnathan@gmail.com>
2024-09-21 16:06:15 +0200

    tp3-vf

--- tp3/prixTuring.c
+++ tp3/prixTuring.c
@@ -74,32 +74,6 @@ void readWinners(FILE *dataFile, Gagnants *listeGagnants){
     }
 }
 
-// void printWinners(FILE *output, Gagnants *listeGagnants){
-// 	rewind(output);
-// 	char *annee, *nom, *sujet, *ligne;
-// 	ligne = malloc(1024*sizeof(char));
-
-// 	for(int i = 0 ; i < listeGagnants->nombreGagnants; i++){
-// 		char charAnnee[4];
-// 		sprintf(charAnnee, "%d", listeGagnants->gagnants[i].annee);
-// 		annee = charAnnee;
-// 		nom = listeGagnants->gagnants[i].nom;
-// 		sujet = listeGagnants->gagnants[i].sujet;
-
-// 		strcat(ligne, annee);
-// 		strcat(ligne,";");
-// 		strcat(ligne, nom);
-// 		strcat(ligne,";");
-// 		strcat(ligne, sujet);
-// 		strcat(ligne,"\n");
-
-// 		printf("%s", ligne);
-
-// 	}
-
-// 	free(ligne);
-	
-// }
 
 void printWinners(FILE *output, Gagnants *listeGagnants){
     rewind(output);
@@ -117,12 +91,30 @@ void printWinners(FILE *output, Gagnants *listeGagnants){
         sprintf(ligne, "%s;%s;%s\n", charAnnee, listeGagnants->gagnants[i].nom, listeGagnants->gagnants[i].sujet);
         
         fputs(ligne, output);
-        printf("%s", ligne);
+		printf("%s", ligne);
     }
 
 	free(ligne);
 }
 
+void infosAnnee(Gagnants *listeGagnants, unsigned int annee){
+	for(int i = 0 ; i < listeGagnants->nombreGagnants ; i++){
+		if(listeGagnants->gagnants[i].annee == annee){
+			printf("L'annee %d, le(s) gagnant(s) ont été : %s \nNature des travaux : %s",annee, listeGagnants->gagnants[i].nom, listeGagnants->gagnants[i].sujet);
+		}
+	}
+}
+
+
+unsigned int cherchePremiereAnnee(Gagnants *listeGagnants){
+	unsigned int res = listeGagnants->gagnants[0].annee;
+	for(int i = 1 ; i < listeGagnants->nombreGagnants ; i++){
+		if (res > listeGagnants->gagnants[i].annee){
+			res = listeGagnants->gagnants[i].annee;
+		}
+	}
+	return res;
+}
 void afficheWinners(Gagnants *listeGagnants){
 	for(int i = 0 ; i < listeGagnants->nombreGagnants; i++){
 		printf("annee : %d \n", listeGagnants->gagnants[i].annee);
@@ -131,33 +123,101 @@ void afficheWinners(Gagnants *listeGagnants){
 		printf("\n");
 	}
 }
+void sortTuringWinnersByYear(Gagnants *listeGagnants){
+	unsigned int anneeEnCours = cherchePremiereAnnee(listeGagnants);
+	printf("annee = %d \n",anneeEnCours);
+	Gagnants tempG;
+	Gagnants *temp = &tempG;
+	temp->nombreGagnants = listeGagnants->nombreGagnants;
+	temp->gagnants = malloc((temp->nombreGagnants)*sizeof(Gagnant));
+
+	for(int i = 0 ; i < listeGagnants->nombreGagnants ; i++){
+		for(int j = 0 ; j < listeGagnants->nombreGagnants ; j++){
+			if(listeGagnants->gagnants[j].annee == (anneeEnCours+i)){
+				temp->gagnants[i].annee = listeGagnants->gagnants[j].annee;
+				temp->gagnants[i].nom = listeGagnants->gagnants[j].nom;
+				temp->gagnants[i].sujet = listeGagnants->gagnants[j].sujet;
+			}
+		}
+	}
+
+	//Copie du temp dans la liste gagnants initiale
+	for(int j = 0 ; j < listeGagnants->nombreGagnants ; j++){
+			listeGagnants->gagnants[j].annee = temp->gagnants[j].annee;
+			listeGagnants->gagnants[j].nom = temp->gagnants[j].nom;
+			listeGagnants->gagnants[j].sujet = listeGagnants->gagnants[j].sujet;
+		}
+}
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // MAIN
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 int main(int argc, char** argv)
 {
-	char filename[] = "turingWinners.csv";
-	char outputFilename[] = "out.csv";
+    char *filename = NULL;
+    char *outputFilename = NULL;
+    int annee = -1; 
+    int sort = 0; 
 
-    FILE *dataFile, *outputFile;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-o") == 0) {
+            if (i + 1 < argc) {
+                outputFilename = argv[++i];  
+            } else {
+                fprintf(stderr, "L'option -o nécessite un nom de fichier.\n");
+                return EXIT_FAILURE;
+            }
+        } else if (strcmp(argv[i], "--info") == 0) {
+            if (i + 1 < argc) {
+                annee = atoi(argv[++i]);  
+            } else {
+                fprintf(stderr, "L'option --info nécessite une année.\n");
+                return EXIT_FAILURE;
+            }
+        } else if (strcmp(argv[i], "--sort") == 0) {
+            sort = 1;  
+        } else {
+            filename = argv[i];  
+        }
+    }
 
-	dataFile = fopen(filename,"r"); 
-	outputFile = fopen(outputFilename,"w"); 
-	
-	printf("nombre de gagnants : %d \n",numberOfWinners(dataFile));
+    if (filename == NULL) {
+        fprintf(stderr, "Aucun fichier d'entrée spécifié.\n");
+        return EXIT_FAILURE;
+    }
 
-	//readWinners
-	Gagnants listeGagnants;
-	readWinners(dataFile, &listeGagnants);
+    FILE *dataFile = fopen(filename, "r");
+    if (dataFile == NULL) {
+        perror("Erreur lors de l'ouverture du fichier d'entrée");
+        return EXIT_FAILURE;
+    }
 
-	//afficheWinners(&listeGagnants);
-	printWinners(outputFile, &listeGagnants);
+    Gagnants listeGagnants;
+    readWinners(dataFile, &listeGagnants);
+    fclose(dataFile);
 
-	fclose(dataFile); 
-	fclose(outputFile); 
+    if (annee != -1) {
+        infosAnnee(&listeGagnants, annee);  
+    }
 
-	free(listeGagnants.gagnants);
+    if (sort == 1) {
+        sortTuringWinnersByYear(&listeGagnants);
+    }
 
-	return EXIT_SUCCESS;
+    if (outputFilename != NULL) {
+        FILE *outputFile = fopen(outputFilename, "w");
+        if (outputFile == NULL) {
+            perror("Erreur lors de l'ouverture du fichier de sortie");
+            free(listeGagnants.gagnants); 
+            return EXIT_FAILURE;
+        }
+
+        printWinners(outputFile, &listeGagnants);  
+        fclose(outputFile);  
+    }
+
+    free(listeGagnants.gagnants);
+
+    return EXIT_SUCCESS;
 }
